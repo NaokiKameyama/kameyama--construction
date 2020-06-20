db4ae8443a42bceb4e3f1b275239809627df4ddf
Naoki-Kameyama <atukan0930@gmail.com>
2020-06-21 01:49:55 +0900

    test

---
+++ kameyama-construction/src/components/App/script.js
@@ -0,0 +1,13 @@
+// import HelloWorld from './components/HelloWorld';
+
+export default {
+    name: "App",
+  
+    components: {
+      // HelloWorld,
+    },
+  
+    data: () => ({
+      //
+    })
+  };
